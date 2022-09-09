0cfdbbaab58e352d8054ea2a2f1ab317bc55ac45
huynheth <huynheth@urbandaleschools.com>
2022-09-09 09:19:27 -0500

    jedi training sep 9

--- 1.0_Jedi_Training.py
+++ 1.0_Jedi_Training.py
@@ -3,9 +3,9 @@ Sign your name:Ethan Huynh
 
 
 
-Define Forking: Making a copy from the repository
+Define Forking: Making a copy from the repository to my own repository
 
-Define Cloning: Making a copy
+Define Cloning: Making a copy from the repository to open on pycharm
 
 Define Branching: making a copy of your code that you can edit without changing the "master" code
 
@@ -13,9 +13,9 @@ Define Committing: making a "hard save"
 
 Define Merging: putting the code that you edit on the new copy of code into your "master" code
 
-Define Pushing:
+Define Pushing: pushing my new modified code to the gitub repository
 
-Define Pull Request:
+Define Pull Request: sending my modified code from my repository back to the teachers
 
 
 '''
